bf72232f28f73b49990d7db50a2482f6e96aa270
Syekh Maulana Wijaya <sahlan.royale@gmail.com>
2023-03-31 16:43:34 +0700

    Mendefinisikan beberapa variable untuk kode MainActivity.kt

--- app/src/main/java/com/example/savingtime/MainActivity.kt
+++ app/src/main/java/com/example/savingtime/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.savingtime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.savingtime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculate() }
     }
+
+    private fun calculate() {
+        // Takes user input as string
+        val startingAmountString = binding.startingAmountEditText.text.toString()
+        val monthlyContributionString = binding.monthlyContributionsEditText.text.toString()
+        val durationInMonthString = binding.durationEditText.text.toString()
+        val interestString = binding.interestEditText.text.toString()
+
+        // Define data type of the input
+        val startingAmount = startingAmountString.toDouble()
+        val monthlyContribution = monthlyContributionString.toDouble()
+        val durationInMonth = durationInMonthString.toInt()
+        val interest = interestString.toDouble()
+
+        // Define temp var
+        var totalAmount = startingAmount
+    }
+
 }
