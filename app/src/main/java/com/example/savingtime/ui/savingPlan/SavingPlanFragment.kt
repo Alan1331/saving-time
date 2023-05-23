2c61b8587fe18230d52727773ba955da3d64b1cb
Syekh Maulana Wijaya <sahlan.royale@gmail.com>
2023-05-24 02:30:10 +0700

    Menambahkan grid view untuk fragment saving plan dan menyimpan preference nya menggunakan DataStore

--- app/src/main/java/com/example/savingtime/ui/savingPlan/SavingPlanFragment.kt
+++ app/src/main/java/com/example/savingtime/ui/savingPlan/SavingPlanFragment.kt
@@ -2,21 +2,32 @@ package com.example.savingtime.ui.savingPlan
 
 import android.os.Bundle
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.asLiveData
 import com.example.savingtime.R
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.savingtime.data.SettingsDataStore
+import com.example.savingtime.data.dataStore
 import com.example.savingtime.databinding.FragmentSavingPlanBinding
 import com.example.savingtime.db.SavingPlanDb
 import com.example.savingtime.db.SavingPlanEntity
 import com.example.savingtime.db.SavingPlanRepository
-import com.example.savingtime.ui.home.HomeViewModelFactory
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.launch
 import java.text.NumberFormat
 import java.util.*
 
 class SavingPlanFragment : Fragment(), ItemClickListener {
+
+    private val layoutDataStore: SettingsDataStore by lazy {
+        SettingsDataStore(requireActivity().dataStore)
+    }
 
     private val viewModel: SavingPlanViewModel by lazy {
         val db = SavingPlanDb.getInstance(requireContext())
@@ -27,6 +38,7 @@ class SavingPlanFragment : Fragment(), ItemClickListener {
 
     private lateinit var binding: FragmentSavingPlanBinding
     private lateinit var myAdapter: SavingPlanAdapter
+    private var isLinearLayout = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,6 +57,13 @@ class SavingPlanFragment : Fragment(), ItemClickListener {
             adapter = myAdapter
             setHasFixedSize(true)
         }
+
+        layoutDataStore.preferenceFlow.asLiveData().observe(viewLifecycleOwner) {
+            isLinearLayout = it
+            setLayout()
+            activity?.invalidateOptionsMenu()
+        }
+
         viewModel.data.observe(viewLifecycleOwner, {
             binding.emptyView.visibility = if (it.isEmpty())
                 View.VISIBLE else View.GONE
@@ -53,8 +72,10 @@ class SavingPlanFragment : Fragment(), ItemClickListener {
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.saving_plan_menu, menu)
+
+        val menuItem = menu.findItem(R.id.action_switch_layout)
+        setIcon(menuItem)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -62,7 +83,30 @@ class SavingPlanFragment : Fragment(), ItemClickListener {
             hapusData()
             return true
         }
+
+        if (item.itemId == R.id.action_switch_layout) {
+            lifecycleScope.launch {
+                layoutDataStore.saveLayout(!isLinearLayout, requireContext())
+            }
+            return true
+        }
+
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun setLayout() {
+        binding.recyclerView.layoutManager = if (isLinearLayout)
+            LinearLayoutManager(context)
+        else
+            GridLayoutManager(context, 2)
+    }
+
+    private fun setIcon(menuItem: MenuItem) {
+        val iconId = if (isLinearLayout)
+            R.drawable.ic_grid_view
+        else
+            R.drawable.ic_view_list
+        menuItem.icon = ContextCompat.getDrawable(requireContext(), iconId)
     }
 
     private fun hapusData() {
